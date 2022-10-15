c5fc839316309f16cca04ebd176fec830d88d1e2
h-dew <benjaminscown@gmail.com>
2022-10-14 20:15:37 -0700

    finished q1

---
+++ 62071873_A4/src/Q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) throws Exception {
+        // Prompts user to input char and checks if it is a vowel
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+
+        char ch = sc.next().charAt(0);
+        
+        if (!Character.isLetter(ch)) {
+
+            System.out.println("Invalid input");
+
+        } else {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("Vowel");
+
+            } else {
+                System.out.println("Consonant");
+
+            }
+        }
+    }
+}
